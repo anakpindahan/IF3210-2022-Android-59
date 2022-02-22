c590e7ce465079d46a12744c56bb98f9cfdbaf9e
mhmmdjafarg <mjafarashadiq712@gmail.com>
2022-02-22 13:58:17 +0700

    complete bottom navbar

--- app/src/main/java/com/pbd/perludilindungi/fragments/VaksinLocationFragment.kt
+++ app/src/main/java/com/pbd/perludilindungi/fragments/VaksinLocationFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import com.pbd.perludilindungi.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,16 +20,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class VaksinLocationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -38,23 +34,11 @@ class VaksinLocationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_vaksin_location, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment VaksinLocationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            VaksinLocationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val locationText : TextView = view.findViewById(R.id.location_text)
+        locationText.setOnClickListener{
+            Toast.makeText(context, "Lokasi Vaksin Fragment", Toast.LENGTH_SHORT).show()
+        }
     }
 }

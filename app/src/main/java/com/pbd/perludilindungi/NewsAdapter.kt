1d91f80adc9b4e7949024369075935f82d9b5149
mhmmdjafarg <mjafarashadiq712@gmail.com>
2022-02-23 22:43:05 +0700

    complete webview and adapter news

---
+++ app/src/main/java/com/pbd/perludilindungi/NewsAdapter.kt
@@ -0,0 +1,52 @@
+package com.pbd.perludilindungi
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+
+class NewsAdapter(val results: ArrayList<News>, val listener: OnAdapterListener) : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.adapter_news, parent, false)
+    )
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val result = results[position]
+        holder.tvTitle.text = result.title
+        holder.tvDate.text = result.pubDate
+        Glide.with(holder.view)
+            .load(result.enclosure["_url"])
+            .placeholder(R.drawable.img_placeholder)
+            .error(R.drawable.img_placeholder)
+            .centerCrop()
+            .into(holder.imgPhoto)
+
+        holder.view.setOnClickListener{
+            listener.onClick(result)
+        }
+    }
+
+    override fun getItemCount() = results.size
+
+    class ViewHolder (val view: View) : RecyclerView.ViewHolder(view) {
+        var imgPhoto: ImageView = view.findViewById(R.id.img_item_photo)
+        var tvTitle: TextView = view.findViewById(R.id.tv_item_title)
+        var tvDate: TextView = view.findViewById(R.id.tv_item_date)
+    }
+
+    fun setData (data : List<News>){
+        results.clear()
+        results.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    interface OnAdapterListener {
+        fun onClick(result : News)
+    }
+}
--- app/src/main/java/com/pbd/perludilindungi/NewsModel.kt
+++ app/src/main/java/com/pbd/perludilindungi/NewsModel.kt
@@ -1,5 +1,5 @@
 package com.pbd.perludilindungi
 
-data class NewsModel ( val success: String, val message: String?, val count_total: String, val results: List<News> )
+data class NewsModel ( val success: Boolean, val message: String?, val count_total: String, val results: List<News> )
 
 data class News(val title: String, val link: List<String>, val pubDate: String, val enclosure: Map<String, String>)
